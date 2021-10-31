ab6c7dbf706d01ca5685db8da71a54ab7f5800ce
Wanderson <wandersonsousa3004@gmail.com>
2021-10-31 20:31:46 -0300

    feat: update searchBar styles

--- src/components/SocialSearchBar/index.jsx
+++ src/components/SocialSearchBar/index.jsx
@@ -4,62 +4,33 @@ import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
-
-import { useTheme } from '@mui/styles';
-import { createStyles, makeStyles } from '@mui/styles';
 import { styled } from '@mui/material/styles';
-
-const PREFIX = 'SocialSearchBar';
-const classes = {
-    root: `${PREFIX}-root`,
-    cta: `${PREFIX}-cta`,
-    content: `${PREFIX}-content`,
-}
-const Root = styled('div')(({ theme }) => ({
-    [`&.${classes.root}`]: {
-        padding: '2px 4px',
-        display: 'flex',
-        alignItems: 'center',
-        width: ({ width }) => width || 300,
-    },
-    [`& .${classes.input}`]: {
-        marginLeft: theme.spacing(1),
-        flex: 1
-    },
-    [`& .${classes.iconButton}`]: {
-        padding: 10,
-    },
-    [`& .${classes.divider}`]: {
-        height: 28,
-        margin: 4,
-    },
-}))
-
-
 
 export default function SocialSearchBar({ width, search, setSearch, submitSearch }) {
     return (
-        <Root width={width}>
-            <Paper className={classes.root}>
-                <InputBase
-                    className={classes.input}
-                    placeholder="Pesquisar..."
-                    inputProps={{ 'aria-label': 'search' }}
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        submitSearch(search);
-                    }}
-                />
-                <IconButton className={classes.iconButton} aria-label="search" onClick={(e) => {
+        <Paper sx={{
+            padding: '4px 4px',
+            margin: '0 4px',
+            display: 'flex',
+            alignItems: 'center'
+        }}>
+            <InputBase
+                placeholder="Pesquisar..."
+                inputProps={{ 'aria-label': 'search' }}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onSubmit={(e) => {
                     e.preventDefault();
                     submitSearch(search);
                 }}
-                >
-                    <SearchIcon />
-                </IconButton>
-            </Paper>
-        </Root>
+            />
+            <IconButton aria-label="search" onClick={(e) => {
+                e.preventDefault();
+                submitSearch(search);
+            }}
+            >
+                <SearchIcon />
+            </IconButton>
+        </Paper>
     );
 }
